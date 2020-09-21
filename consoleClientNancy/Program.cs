06453dd5eae51ea1217cd23ade5a2ef681e86380
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-21 14:42:21 +0200

    added console client (simple) for nancy server

---
+++ consoleClientNancy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace consoleClientNancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:5000/getByAge/27";
+            WebClient wc = new WebClient();            
+            string jsonContent = wc.DownloadString(url);
+
+            System.Console.WriteLine(jsonContent);
+            
+        }
+    }
+}
