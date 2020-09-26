3b161e19a75345d4c0d736d471942597e0d258b9
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-26 17:26:08 +0200

    async await materials added

---
+++ 05-ASYNC-AWAIT/01-asyncawait/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07asyncawait
+{
+    class Worker
+    {
+        public bool IsComplete { get; private set; }
+
+        public async void DoWork()
+        {
+            IsComplete = false;
+            Console.WriteLine("\t... doing my work");
+            await LongOperation();
+            Console.WriteLine("\t... my work is done");
+            IsComplete = true;
+        }
+
+        private Task LongOperation()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+               Console.WriteLine("\t >> long operation is under progress...");
+               Thread.Sleep(3000);
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started...");
+
+            var w = new Worker();
+            w.DoWork();
+
+            while (!w.IsComplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(250);
+            }
+
+            Console.WriteLine("Finished...");
+            Console.ReadLine();
+
+        }
+    }
+}
